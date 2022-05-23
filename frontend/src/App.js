005d34e132d2647be805056d1c2154369bb4c7b2
pexyke <pekopeter0@gmail.com>
2022-05-23 23:21:15 +0200

    basic sidebar with tailwind css added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,7 +16,7 @@ const App = () => {
           <div className='flex relative dark:bg-main-dark-bg'>
             <div className='fixed right-4 bottom-4' style={{ zIndex: '1000S' }}>
              <TooltipComponent content="Settings" position='Top'>
-               <button type='button' className='text-3x1 p-3 hover:drop-shadow-x1 hover:bg-light-gray text-white' style={{ background: 'blue', borderRadius: '50%'}}>
+               <button type='button' className='text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white' style={{ background: 'blue', borderRadius: '50%'}}>
                  <FiSettings />
                </button>
              </TooltipComponent>
