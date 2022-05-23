fc3a13da7fb4b5dcaf9d48288b3bfb65ba009ae6
pexyke <pekopeter0@gmail.com>
2022-05-23 21:56:33 +0200

    basic file structure builded

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
+import { SiShopware } from 'react-icons/si'
+import { MdOutlineCancel } from 'react-icons/md'
+import { ToolTipComponent } from '@syncfusion/ej2-react-popups'
 
 const Sidebar = () => {
   return (
