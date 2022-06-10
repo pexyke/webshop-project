7429fb7242897ce9425f376b8093789596fd1352
pexyke <pekopeter0@gmail.com>
2022-06-10 15:07:35 +0200

    ContextProvider added

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -6,9 +6,10 @@ import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
 //object check this
 import { links } from "../data/dummy";
+import { useStateContext } from "../contexts/ContextProvider";
 
 const Sidebar = () => {
-  const activeMenu = true;
+  const { activeMenu, setActiveMenu } = useStateContext();
 
   const activeLink =
     "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2";
@@ -23,7 +24,7 @@ const Sidebar = () => {
           <div className="flex justify-between items-center">
             <Link
               to="/"
-              onClick={() => {}}
+              onClick={() => setActiveMenu(false)}
               className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900"
             >
               <SiShopify /> <span>Shoppy</span>
@@ -31,8 +32,8 @@ const Sidebar = () => {
             <TooltipComponent content="Menu" position="BottomCenter">
               <button
                 type="button"
-                onClick={() => {}}
-                className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden"
+                onClick={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)}
+                  className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden"
               >
                 <MdOutlineCancel />
               </button>
